e2ec7dc9808ed5039c6732b40733fa20380f79ac
applephii <khaqiki@gmail.com>
2025-06-05 08:29:02 +0700

    Update

--- index.js
+++ index.js
@@ -25,5 +25,6 @@ app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, '0.0.0.0', () => {
-    console.log(`Server running on port ${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 });
+
