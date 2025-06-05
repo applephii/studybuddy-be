47039fe71035417defe4fdf082a3a18ba16b73a7
applephii <khaqiki@gmail.com>
2025-06-05 08:23:06 +0700

    Update

--- index.js
+++ index.js
@@ -18,6 +18,12 @@ app.use(taskRouter);
 app.use(favPlaceRouter);
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
-app.listen(3000, '0.0.0.0', () => {
-    console.log("Server is connected...");
+// app.listen(3000, '0.0.0.0', () => {
+//     console.log("Server is connected...");
+// });
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server running on port ${PORT}`);
 });
